63e653769d5cc00eef623499d2fb98bc4dc64ac9
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-04-21 03:31:59 -0300

    fix(switcherbutton.js): remove the useState() function
    I had to use a little of VanillaJS code. I need to understand how to preserve the state, because of
    the others pages.

--- rest-countries-api/src/components/SwitcherButton/index.js
+++ rest-countries-api/src/components/SwitcherButton/index.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './themes/dark.scss';
+
 
 function SwitcherButton(){
-    const [switcher, setSwitcher] = useState(false);
     let switcherClass = "switcher-btn";
-    function handleSwitcherButton() {
-        setSwitcher(switcher + true)
+
+    function handleSwitcherButton(event) {
+        const value = event.target;
+        if(value.checked === true){
+            document.body.style.backgroundColor = "#000000";
+            console.log('claro');
+        } else{
+            document.body.style.backgroundColor = "#efefef";
+            console.log('escuro');
+        }
     }
     return (
         <div className="switcher-button">
             <input className={switcherClass} 
-            onChange={handleSwitcherButton} 
-            defaultChecked
-            type="checkbox" name="switcher" id="switcher-btn"/>
+            onChange={handleSwitcherButton}
+            type="checkbox" 
+            name="switcher" 
+            id="switcher-btn"
+                hidden
+            />
 
             <label  htmlFor="switcher-btn">
                 <ion-icon name="moon-outline"></ion-icon>
